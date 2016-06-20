9c30b35c37f63dd80cda606201e8cc98bbea14d8
laliberte <laliberte.frederic@gmail.com>
2016-06-20 12:08:51 -0400

    Removed continuous months feature because it was buggy

--- lib/read_soft_links.py
+++ lib/read_soft_links.py
@@ -388,30 +388,31 @@ def time_restriction_months(options,date_axis,time_restriction_for_years):
         #time_restriction=np.logical_and(time_restriction,months_axis==month)
         time_restriction=np.logical_and(time_restriction_for_years,[True if month in options.month else False for month in months_axis])
         #Check that months are continuous:
-        if options.month==[item for item in options.month if (item % 12 +1 in options.month or item-2% 12+1 in options.month)]:
-            time_restriction_copy=copy.copy(time_restriction)
-            #Months are continuous further restrict time_restriction to preserve continuity:
-            if time_restriction[0] and months_axis[0]-2 % 12 +1 in options.month:
-                time_restriction[0]=False
-            if time_restriction[-1] and months_axis[-1] % 12 +1 in options.month:
-                time_restriction[-1]=False
-
-            for id in range(len(time_restriction))[1:-1]:
-                if time_restriction[id]:
-                    if (( ((months_axis[id-1]-1)-(months_axis[id]-1)) % 12 ==11 or
-                         months_axis[id-1] == months_axis[id] ) and
-                        not time_restriction[id-1]):
-                        time_restriction[id]=False
-
-            for id in reversed(range(len(time_restriction))[1:-1]):
-                if time_restriction[id]:
-                    if (( ((months_axis[id+1]-1)-(months_axis[id]-1)) % 12 ==1 or
-                         months_axis[id+1] == months_axis[id] ) and
-                        not time_restriction[id+1]):
-                        time_restriction[id]=False
-            #If all values were eliminated, do not ensure continuity:
-            if not np.any(time_restriction):
-                time_restriction=time_restriction_copy
+        #if options.month==[item for item in options.month if (item % 12 +1 in options.month or item-2% 12+1 in options.month)]:
+        #    time_restriction_copy=copy.copy(time_restriction)
+        #    #Months are continuous further restrict time_restriction to preserve continuity:
+        #    if time_restriction[0] and months_axis[0]-2 % 12 +1 in options.month:
+        #        time_restriction[0]=False
+        #    if time_restriction[-1] and months_axis[-1] % 12 +1 in options.month:
+        #        time_restriction[-1]=False
+        #
+        #    for id in range(len(time_restriction))[1:-1]:
+        #        if time_restriction[id]:
+        #            if (( ((months_axis[id-1]-1)-(months_axis[id]-1)) % 12 ==11 or
+        #                 months_axis[id-1] == months_axis[id] ) and
+        #                not time_restriction[id-1]):
+        #                time_restriction[id]=False
+        #
+        #    for id in reversed(range(len(time_restriction))[1:-1]):
+        #        if time_restriction[id]:
+        #            if (( ((months_axis[id+1]-1)-(months_axis[id]-1)) % 12 ==1 or
+        #                 months_axis[id+1] == months_axis[id] ) and
+        #                not time_restriction[id+1]):
+        #                time_restriction[id]=False
+        #    print(date_axis[time_restriction])
+        #    #If all values were eliminated, do not ensure continuity:
+        #    if not np.any(time_restriction):
+        #        time_restriction=time_restriction_copy
         return time_restriction
     else:
         return time_restriction_for_years
