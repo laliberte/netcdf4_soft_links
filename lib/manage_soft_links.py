6b025a8d95b4e2d89d4440aa1eb0b255a3f67f4e
laliberte <laliberte.frederic@gmail.com>
2016-06-22 11:23:36 -0400

    version change

--- lib/manage_soft_links.py
+++ lib/manage_soft_links.py
@@ -11,7 +11,7 @@ def main(string_call=None):
     import textwrap
 
     #Option parser
-    version_num='0.5.2'
+    version_num='0.5.3'
     description=textwrap.dedent('''\
     This script aggregates soft links to OPENDAP or local files.\
     ''')
