a8380a24122542b72b1fd18f038e707265796454
laliberte <laliberte.frederic@gmail.com>
2016-11-06 22:14:20 -0500

    Readme

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
                             'h5py',
                             'h5netcdf>=0.3',
                             'netCDF4',
-                            'netcdf4_pydap>=0.1.3',
+                            'netcdf4_pydap>=0.2',
                             'requests>=1.1.0',
                             'requests_cache'
                             ],
