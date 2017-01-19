b7c66806184d6beac9307a44da4b57abef6fd0bf
laliberte <laliberte.frederic@gmail.com>
2017-01-19 15:42:01 -0500

    Some clean-up and netcdf4_pydap integration

--- netcdf4_soft_links/netcdf4_pydap/__init__.py
+++ netcdf4_soft_links/netcdf4_pydap/__init__.py
@@ -1,1 +1,4 @@
-from .netcdf4_pydap.netcdf4_pydap import Dataset
+from .netcdf4_pydap.netcdf4_pydap import Dataset, http_Dataset
+from .netcdf4_pydap.netcdf4_pydap.httpserver import RemoteEmptyError
+
+__all__ = [Dataset, http_Dataset, RemoteEmptyError]
